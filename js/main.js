f4b6454ef81e670f10254abb70efa641e9c70744
ShyKoo <kostyarondo@gmail.com>
2026-04-05 18:01:11 +0300

    nav + btn changed

--- js/main.js
+++ js/main.js
@@ -13,6 +13,11 @@
     const burger_btn = document.querySelector('.header__burger')
     const header_panel = document.querySelector('.header__nav')
     const burger_close = document.querySelector('.header__close-btn')
+    const header_element = document.querySelectorAll('.header__item')
     burger_btn.addEventListener('click', () => {header_panel.classList.add('header__nav_active')})
     burger_close.addEventListener('click', () => {header_panel.classList.remove('header__nav_active')})
+
+    for (let element of header_element) {
+        element.addEventListener('click', () => {header_panel.classList.remove('header__nav_active')})
+    }
 }())
